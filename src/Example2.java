db388f4fb7643faf11fc197a5a534f0c49d8d35f
Daniel <v.marmuta2000@gmail.com>
2026-04-18 18:38:18 +0200

    Example2

---
+++ src/Example2.java
@@ -0,0 +1,10 @@
+public class Example2 {public static void main(String[] args) {
+    int myVar1; // variable declaration
+    int myVar2; // declaring another variable
+    myVar1 = 1024; // assigning the value 1024 to the variable myVarl
+    System.out.println("myVar1 contains" + myVar1);
+    myVar2 = myVar1 / 2;
+    System.out.print("myVar2 contains : myVarl / 2:");
+    System.out.println(myVar2);
+}
+}
