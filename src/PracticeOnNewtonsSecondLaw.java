28895c1fead629f32b8eb274ce04964c8575cdc6
Daniel <v.marmuta2000@gmail.com>
2026-04-19 19:58:57 +0200

    PracticeOnNewtonsSecondLaw

---
+++ src/PracticeOnNewtonsSecondLaw.java
@@ -0,0 +1,12 @@
+public class PracticeOnNewtonsSecondLaw {
+    public static void main(String[] args) {
+    double a, m, F;
+    m = 132.5;
+    a = 5.3;
+    F = a * m;
+    System.out.println("mass = " + m);
+    System.out.println("acceleration = " + a);
+    System.out.println("Force = accelertion + mass " + F);
+    }
+}
+}
