9fe570397cda6eb89ccc38cd4dfaaf94652cc9e3
Daniel <v.marmuta2000@gmail.com>
2026-04-19 18:17:53 +0200

    Example2FromMemory

---
+++ src/Example2FromMemory.java
@@ -0,0 +1,12 @@
+public class Example2FromMemory {public static void main(String[] args) {
+    int myApples, yourApples, usApples, applesForEach;
+    myApples = 380;
+    yourApples = 120;
+    System.out.print("I have " + myApples + " apples ");
+    System.out.print("and you have " + yourApples);
+    usApples = myApples + yourApples;
+    System.out.print(", so our total is " + usApples);
+    applesForEach = usApples / 2;
+    System.out.print(", with " + applesForEach + " apples each");
+}
+}
