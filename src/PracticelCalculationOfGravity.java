c4306fba77268e2f5b91b373d08dfe5768a8e48c
Daniel <v.marmuta2000@gmail.com>
2026-04-22 01:49:57 +0200

    Moon weight calculator

---
+++ src/PracticelCalculationOfGravity.java
@@ -0,0 +1,9 @@
+public class PracticelCalculationOfGravity {
+    public static void main(String[] args) {
+        double Kg, KgOnMoon;
+        Kg = 71;
+        System.out.println("The gravity on the moon is about 17% of Earth's, so multiplying our weight by 0.17 gives us our weight if we were on the moon.");
+        KgOnMoon = Kg * 0.17;
+        System.out.println("If we weigh 71 on Earth, then on the moon the value = "+ KgOnMoon);
+    }
+}
