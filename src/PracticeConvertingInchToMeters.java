927d4fd82ef8710562efb1c56f5121e8ec7b3031
Daniel <v.marmuta2000@gmail.com>
2026-04-23 19:24:21 +0200

    inches to meters

--- src/PracticeConvertingInchToMeters.java
+++ src/PracticeConvertingInchToMeters.java
@@ -1,14 +1,15 @@
 public class PracticeConvertingInchToMeters {
     public static void main(String[] args){
-        double Inch, Meters;
+        double Inch, Meters, Third;
         int counter;
         counter = 0;
-        for (Inch = 0; Inch <= 144; Inch++) {
+        for (Inch = 1; Inch <= 144; Inch++) {
             Meters = Inch * 0.0254;
+            Third = Inch % 12;
             System.out.println(Inch + " Inch = " + Meters + " meters");
 
         counter++;
-        if (Inch == 12) {
+        if (Third == 0) {
             System.out.println();
         }
         }
