57c3145a899a1c735d5a709484875178ba641189
Daniel <v.marmuta2000@gmail.com>
2026-04-23 20:01:58 +0200

    Practice inches to meters

--- src/PracticeConvertingInchToMeters.java
+++ src/PracticeConvertingInchToMeters.java
@@ -1,15 +1,20 @@
 public class PracticeConvertingInchToMeters {
     public static void main(String[] args){
-        double Inch, Meters, Third;
-        int counter;
-        counter = 0;
-        for (Inch = 1; Inch <= 144; Inch++) {
-            Meters = Inch * 0.0254;
-            Third = Inch % 12;
-            System.out.println(Inch + " Inch = " + Meters + " meters");
+        int Inch, Third, Four; //counter → int
+        double Meters; //calculation result → double
 
-        counter++;
-        if (Third == 0) {
+
+        for (Inch = 2; Inch <= 144; Inch++) {
+
+            Meters = Inch * 0.0254;
+            Third = Inch % 12; //the variable is included only if the number is 0 when divided by 12
+            Four = Inch % 2; //only even inches are printed
+
+            if (Four == 0) System.out.println
+                    (Inch + " Inch = "
+                    + Meters + " meters");
+
+        if (Third == 0) { //space after every 12th inch
             System.out.println();
         }
         }
