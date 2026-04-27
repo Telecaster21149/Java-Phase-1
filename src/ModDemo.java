31b2b683dbdd5275ef5666e8d09c25b98c2a2087
Daniel <v.marmuta2000@gmail.com>
2026-04-27 17:13:22 +0200

    Scope

---
+++ src/ModDemo.java
@@ -0,0 +1,15 @@
+public class ModDemo {
+    public static void main(String[] args) {
+        int iresult, iren;
+        double dresult, dren;
+
+        iresult = 10 / 3;
+        iren = 10 % 3;
+
+        dresult = 10.0 / 3;
+        dren = 10.0 % 3;
+
+        System.out.println(iresult + " | " + iren);
+        System.out.println(dresult + " | " + dren);
+    }
+}
