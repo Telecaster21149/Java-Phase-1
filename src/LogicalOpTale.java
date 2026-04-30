aae18796b82f9edf2f3cb00e8dce1ac26adade93
Daniel <v.marmuta2000@gmail.com>
2026-04-30 15:47:05 +0200

    LogicTable 1&0

--- src/LogicalOpTale.java
+++ src/LogicalOpTale.java
@@ -7,26 +7,26 @@ public class LogicalOpTale {
 
         p = true; q = true;
         //System.out.println(" P = true; Q = true");
-        System.out.print(p + "\t" + q + "\t");
-        System.out.print((p&q) + "\t" + (p|q) + "\t");
-        System.out.println((p^q) + "\t" + (!p) + "\t");
+        System.out.print((p ? 1 : 0) + "\t" + (q ? 1 : 0) + "\t");
+        System.out.print((p&q?1:0) + "\t" + (p|q?1:0) + "\t");
+        System.out.println((p^q?1:0) + "\t" + (!p?1:0) + "\t");
 
         p = true; q = false;
         //System.out.println(" P = true; Q = false");
-        System.out.print(p + "\t" + q + "\t");
-        System.out.print((p&q) + "\t" + (p|q) + "\t");
-        System.out.println((p^q) + "\t" + (!p));
+        System.out.print((p ? 1 : 0) + "\t" + (q ? 1 : 0) + "\t");
+        System.out.print((p&q?1:0) + "\t" + (p|q?1:0) + "\t");
+        System.out.println((p^q?1:0) + "\t" + (!p?1:0));
 
         p = false; q = true;
         //System.out.println(" P = false; Q = true");
-        System.out.print(p + "\t" + q + "\t");
-        System.out.print((p&q) + "\t" + (p|q) + "\t");
-        System.out.println((p^q) + "\t" + (!p) + "\t");
+        System.out.print((p ? 1 : 0) + "\t" + (q ? 1 : 0) + "\t");
+        System.out.print((p&q?1:0) + "\t" + (p|q?1:0) + "\t");
+        System.out.println((p^q?1:0) + "\t" + (!p?1:0) + "\t");
 
         p = false; q = false;
         //System.out.println(" P = false; Q = false");
-        System.out.print(p + "\t" + q + "\t");
-        System.out.print((p&q) + "\t" + (p|q) + "\t");
-        System.out.println((p^q) + "\t" + (!p));
+        System.out.print((p ? 1 : 0) + "\t" + (q ? 1 : 0) + "\t");
+        System.out.print((p&q?1:0) + "\t" + (p|q?1:0) + "\t");
+        System.out.println((p^q?1:0) + "\t" + (!p?1:0));
     }
 }
