a24ada3f761e83baa61d3fb0b924b86d50c5a725
Daniel <v.marmuta2000@gmail.com>
2026-05-06 16:52:37 +0200

    Keyboard input

---
+++ src/Guess.java
@@ -0,0 +1,10 @@
+public class Guess {
+    public static void main(String[] args)
+    throws java.io.IOException {
+        char ch, answer = 'K';
+        System.out.println("The letter between A and Z is intended");
+        System.out.print("Try to guess it: ");
+        ch = (char) System.in.read();
+        if (ch == answer) System.out.println("Right");
+    }
+}
