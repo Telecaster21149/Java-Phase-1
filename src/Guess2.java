05350ec1b0a4d0684953f227cc93baee00dede7f
Daniel <v.marmuta2000@gmail.com>
2026-05-06 17:03:41 +0200

    Letter Guessing Game 2

---
+++ src/Guess2.java
@@ -0,0 +1,11 @@
+public class Guess2 {
+    public static void main(String[] args)
+        throws java.io.IOException {
+        char ch, answer = 'K';
+        System.out.println("The letter between A and Z is intended");
+        System.out.print("Try to guess it: ");
+        ch = (char) System.in.read();
+        if (ch == answer) System.out.println("**Right**");
+        else System.out.println("...Alas, you didn't guess");
+    }
+}
