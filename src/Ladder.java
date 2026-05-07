4ba497b1c570b29e027fe55b9ffdf99cfcc221d6
Daniel <v.marmuta2000@gmail.com>
2026-05-07 14:54:13 +0200

    if-else-if

---
+++ src/Ladder.java
@@ -0,0 +1,17 @@
+public class Ladder {
+    public static void main(String[] args) {
+        int x;
+        for (x = 1; x < 6; x++) {
+            if (x==1)
+                System.out.println("the value of x is 1");
+            else if (x==2)
+                    System.out.println("the value of x is 2");
+            else if (x==3)
+                    System.out.println("the values of x is 3");
+            else if (x==4)
+                    System.out.println("the value of x is 4");
+            else System.out.println("the value of of x is not between 1 and 4");
+        }
+
+    }
+}
